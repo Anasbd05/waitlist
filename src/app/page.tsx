b5c2fbeb42cab0acb9dd6f4ae1a3cfebace1f9fd
anas bd <anastrying05@gmail.com>
2025-05-22 11:47:56 +0100

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,14 +34,14 @@ const Page = () => {
   return (
     <section className='w-full items-center bg-black text-white flex flex-col h-screen justify-center'>
       <main className='flex w-full items-center flex-col justify-center'>
-        <h2 className='text-3xl font-semibold'>Let AI Design Your Gamer Setup</h2>
-        <p className='text-3xl tracking-wider font-bold bg-gradient-to-r bg-clip-text text-transparent from-orange-400 to-purple-700'>
+        <h2 className='text-xl md:text-3xl font-semibold'>Let AI Design Your Gamer Setup</h2>
+        <p className='text-xl md:text-3xl tracking-wider font-bold bg-gradient-to-r bg-clip-text text-transparent from-orange-400 to-purple-700'>
           Join the Waitlist
         </p>
       </main>
 
       <form onSubmit={submitWaitlist} className='mt-10 flex flex-col gap-4 items-center w-full'>
-        <div className="flex w-2/6 items-center px-4 border border-neutral-800 rounded-md">
+        <div className="flex w-3/5 md:w-4/6 lg:w-3/6 xl:w-2/6 items-center px-4 border border-neutral-800 rounded-md">
           <Mail className='text-neutral-400 w-6 h-6' />
           <input
             value={email}
@@ -55,11 +55,11 @@ const Page = () => {
         <textarea
           value={request}
           onChange={(e) => setRequest(e.target.value)}
-          className='flex w-2/6 items-center py-2 h-48 px-4 text-gray-100 border border-neutral-800 outline-none rounded-md'
+          className='flex w-3/5 md:w-4/6 lg:w-3/6 xl:w-2/6 items-center py-2 h-48 px-4 text-gray-100 border border-neutral-800 outline-none rounded-md'
           placeholder="Features, style, and how much you'd pay — tell us everything!"
         />
 
-        <button type="submit" className='py-2 rounded-lg bg-purple-500 text-white w-2/6 hover:opacity-80 cursor-pointer'>
+        <button type="submit" className='py-2 rounded-lg bg-purple-500 text-white w-3/5 md:w-4/6 lg:w-3/6 xl:w-2/6 hover:opacity-80 cursor-pointer'>
           Join Waitlist
         </button>
       </form>
